fd05a40fe1eb936dff14bc49e44bc344bce49913
Vishal <vishalvoorugonda28@gmail.com>
2025-01-26 18:40:07 +0530

    Image-Search

---
+++ data.js
@@ -0,0 +1,68 @@
+const data=[
+    {    
+        id:1,
+        img:"https://plus.unsplash.com/premium_photo-1667311649552-2cfab63bdcfc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Nature"
+    },
+    {    
+        id:2,
+        img:"https://images.unsplash.com/photo-1626594995085-36b551227b9a?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Tree"
+    },
+    {    
+        id:3,
+        img:"https://images.unsplash.com/photo-1618886487325-f665032b6352?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Water"
+    },
+    {    
+        id:4,
+        img:"https://images.unsplash.com/photo-1622899505135-694e8ccffce8?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Nature"
+    },
+    {    
+        id:5,
+        img:"https://images.unsplash.com/photo-1518495973542-4542c06a5843?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Flower"
+    },
+    {    
+        id:6,
+        img:"https://images.unsplash.com/photo-1444492417251-9c84a5fa18e0?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b3V0ZG9vcnxlbnwwfHwwfHx8MA%3D%3D",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Water Fall"
+    },
+    {    
+        id:7,
+        img:"https://images.unsplash.com/photo-1445108771252-d1cc31a02a3c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8b3V0ZG9vcnxlbnwwfHwwfHx8MA%3D%3D",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Nature"
+
+    },
+    {    
+        id:8,
+        img:"https://images.unsplash.com/photo-1421790500381-fc9b5996f343?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D",
+         href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Outdoor"
+
+    },
+    {    
+        id:9,
+        img:"https://images.unsplash.com/photo-1467385829985-2b0fb82b5193?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG91dGRvb3J8ZW58MHx8MHx8fDA%3D",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Wall Cycle"
+
+    },
+    {    
+        id:10,
+        img:"https://images.unsplash.com/photo-1724931282671-2d3bcd6de8f2?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXx0TktkRUF6R2lVMHx8ZW58MHx8fHx8",
+        href:"https://unsplash.com/photos/a-close-up-of-leaves-on-a-tree-fhydR9NMHdM",
+        name:"Nature with House"
+
+    },
+]
+
+export default data;
